d9d2e55deb5fe877a64fe53d6bfabf73b24839b8
mehmetnuricetin <51626115+mehmetnuricetin@users.noreply.github.com>
2026-06-15 14:23:56 +0200

    Simplify Terraform to single main.tf with random storage account

--- main.tf
+++ main.tf
@@ -4,8 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
     }
-    # Note: Carl references a custom identity provider written by Simon Golag 
-    # to fetch Azure CLI credentials without persisting subscription IDs to state.
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
   }
 }
 
@@ -13,56 +15,21 @@ provider "azurerm" {
   features {}
 }
 
-# Resource Group using string interpolation for naming conventions
 resource "azurerm_resource_group" "this" {
-  name     = "rg-${var.environment}-${var.location_short}-${var.common_name}"
-  location = var.location
+  name     = "rg-drift-demo"
+  location = "swedencentral"
 }
 
-# Virtual Network
-resource "azurerm_virtual_network" "this" {
-  name                = "vnet-${var.environment}-${var.location_short}-${var.common_name}"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
+resource "random_string" "storage_suffix" {
+  length  = 10
+  upper   = false
+  special = false
 }
 
-# Subnet
-resource "azurerm_subnet" "internal" {
-  name                 = "sn-internal"
-  resource_group_name  = azurerm_resource_group.this.name
-  virtual_network_name = azurerm_virtual_network.this.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-# Public IP Address (Standard & Static are requirements for NAT Gateway)
-resource "azurerm_public_ip" "this" {
-  name                = "pip-${var.environment}-${var.location_short}-${var.common_name}"
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-  zones               = ["1"]
-}
-
-# NAT Gateway (Explicitly matched to Zone 1 alongside the Public IP)
-resource "azurerm_nat_gateway" "this" {
-  name                    = "ngw-${var.environment}-${var.location_short}-${var.common_name}"
-  location                = azurerm_resource_group.this.location
-  resource_group_name     = azurerm_resource_group.this.name
-  sku_name                = "Standard"
-  idle_timeout_in_minutes = 4
-  zones                   = ["1"]
-}
-
-# Glue resource: Associating the Public IP to the NAT Gateway
-resource "azurerm_nat_gateway_public_ip_association" "this" {
-  nat_gateway_id       = azurerm_nat_gateway.this.id
-  public_ip_address_id = azurerm_public_ip.this.id
-}
-
-# Glue resource: Associating the Subnet to the NAT Gateway
-resource "azurerm_subnet_nat_gateway_association" "this" {
-  subnet_id      = azurerm_subnet.internal.id
-  nat_gateway_id = azurerm_nat_gateway.this.id
+resource "azurerm_storage_account" "this" {
+  name                     = "drift${random_string.storage_suffix.result}"
+  location                 = azurerm_resource_group.this.location
+  resource_group_name      = azurerm_resource_group.this.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
